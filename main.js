45e5bde4d2e905f59f6268fe300e0e3f89f61bfd
raffimolero <raffimolero@gmail.com>
2024-06-05 23:51:24 +0800

    comments for button functions

--- main.js
+++ main.js
@@ -197,7 +197,6 @@ function resizeGrid(w, h) {
             row.appendChild(makeTile(x, y, w, h));
         }
     }
-    return grid;
 }
 
 // takes in a source rule table, and tries to
@@ -337,7 +336,7 @@ function parseAndAddRule(ruleText) {
 }
 
 // called by a button
-// parses and loads the rule in the rule text box
+// parses and loads the contents of the rule text box
 function getAndParseRule() {
     const rule = parseAndAddRule(document.getElementById('rule').value);
     loadRule(rule);
@@ -447,72 +446,68 @@ function loadRle(rle) {
     loadPattern(pattern, borderless);
 }
 
+// called by a button
+// takes the contents of the rle text box and loads it
 function getAndLoadRle() {
     loadRle(document.getElementById('rle').value);
 }
 
+// sometimes called by a button
+// changes the parity of the margolus grid
 function switchParity() {
     parity ^= 1;
     grid.updateTableBorders();
 }
 
+// steps the simulation once
 function step() {
-    if (
-        funnyAssert(
-            grid,
-            'load something pls',
-            'yes of course lemme just make a step with no feet'
-        )
-    ) {
-        grid.step();
-    }
+    grid.step();
 }
 
-function stepButton() {
+// called by a button
+// steps the simulation, but also pauses it
+function manualStep() {
     step();
     stop();
 }
 
+// pauses the simulation
 function stop() {
+    if (!timerInterval) return;
     document.getElementById('run').innerHTML = 'Start';
     clearInterval(timerInterval);
     timerInterval = null;
 }
 
+// starts running the simulation at the speed set by the 'delay' html element
 function start() {
+    if (timerInterval) return;
     document.getElementById('run').innerHTML = 'Stop';
     timerInterval = setInterval(step, document.getElementById('delay').value);
+    step();
 }
 
+// called by a button
+// starts/stops the simulation
 function toggleRun() {
-    if (
-        !funnyAssert(
-            grid,
-            'load pattern pls',
-            'do you really expect me to run with no legs'
-        )
-    ) {
-        return;
-    }
-
-    if (timerInterval) {
-        stop();
-    } else {
-        step();
-        start();
-    }
+    if (timerInterval) stop();
+    else start();
 }
 
+// sometimes called by a button
+// sets all the cells of the grid's border to the specified color
+// the grid's border is static and does not change during simulation
 function fillBorder(fillColor) {
     if (
         !funnyAssert(
-            grid,
-            'load pattern pls',
+            width * height > 0,
+            'load something that isnt nothing pls',
             'bruv am i really boutta build the great wall of china but without the china'
         )
     ) {
         return;
     }
+
     for (const tile of grid.childNodes[0].childNodes) {
         tile.set(fillColor);
         tile.updateTableBorders();
@@ -529,6 +524,8 @@ function fillBorder(fillColor) {
         row[width - 1].updateTableBorders();
     }
 }
+
+// ===== INIT/MAIN =====
 
 getAndParseRule();
 getAndLoadRle();
