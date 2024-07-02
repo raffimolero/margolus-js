b55b66dbac5ea665a64856d66c08c8a3ea708661
raffimolero <raffimolero@gmail.com>
2024-07-03 01:56:41 +0800

    split lexer and parser

---
+++ lexer.js
@@ -0,0 +1,197 @@
+/**
+ * lexes stuff.
+ *
+ * also handles line and column numbers, as well as error formatting.
+ */
+class Lexer {
+    text;
+    constructor(text) {
+        if (!text) {
+            console.log('WARNING: This lexer does not have any text.');
+        }
+        pront(text);
+        this.text = text;
+    }
+
+    index = 0;
+    line = 1;
+    col = 1;
+    lines = [0];
+    current_token = null;
+    // ordered by how likely i think they would appear.
+    // only exceptions are special-cased identifiers.
+    // neighborhoods like margolus|square4cyclic, and symmetries like
+    regexes = [
+        ['shenanigans', /‮/y],
+        ['whitespace', /[\t\f\cK ]+/y],
+        ['illegal identifier', /\d[-_\w]+/y],
+        ['number', /\d+/y],
+        ['keyword', /var|n_states|neighborhood|symmetries/y],
+        ['neighborhood', /margolus|square4cyclic/y],
+        ['symmetry', /(none)|rot([24])(ref)?|([xy])ref|(diag)/y], // use the capture group to figure out which one
+        ['identifier', /[-_a-zA-Z][-_\w]*/y],
+        // comma, colon, equal, and braces are for syntax
+        // %^&* are indexing characters and were agreed on during this conversation:
+        // https://discord.com/channels/357922255553953794/437055638376284161/1256579184793223198
+        ['punctuation', /[,:={}%^&*]/y],
+        ['newline', /\r?\n/y],
+        ['comment', /#.*/y],
+        ['header', /@[A-Z]*/y],
+        ['unknown', /[\w\W]/y],
+    ];
+
+    token_info(token) {
+        const value = token.value;
+        const out = {
+            length: value.length,
+            line: token.line,
+            col: token.col,
+        };
+        switch (token.kind) {
+            case 'newline':
+                out.value = 'Newline';
+                return out;
+            case 'unknown':
+                const code = value.charCodeAt(0);
+                out.value = `'${value}' (unrecognized character, code: ${code})`;
+                return out;
+            case 'end of file':
+                out.length = 1;
+                out.value = 'End of File';
+                return out;
+            case 'shenanigans':
+                out.value = 'Unicode Reverse Shenanigans';
+                return out;
+            default:
+                out.value = `'${value}' (${token.kind})`;
+                return out;
+        }
+    }
+
+    /** end_line is exclusive. */
+    get_context_lines(start_line, end_line) {
+        // defensive programming
+        if (this.unfinished()) {
+            throw 'attempted to get context lines without lexer finishing.';
+        }
+
+        const clamp = val => Math.max(0, Math.min(this.lines.length - 1, val));
+        const start = this.lines[clamp(start_line)];
+        let end = this.lines[clamp(end_line)];
+        if (this.text[end - 1] === '\n') {
+            end--;
+            if (this.text[end - 1] === '\r') {
+                end--;
+            }
+        }
+        return this.text.slice(start, end);
+    }
+
+    /** returns true if end of file has not been reached. */
+    unfinished() {
+        return this.peek().kind !== 'end of file';
+    }
+
+    /** finishes lexing all tokens, making them accessible to get_context_lines */
+    finish() {
+        while (this.next().kind !== 'end of file') {}
+        this.lines.push(this.index);
+    }
+
+    /**
+     * lexes a whole token based on a regex.
+     *
+     * returns a string representing the regex match,
+     * or null if the string does not start with a match.
+     */
+    try_regex(regex) {
+        regex.lastIndex = this.index;
+        const match = regex.exec(this.text);
+        if (match !== null) {
+            this.index = regex.lastIndex;
+        }
+        return match;
+    }
+
+    /** returns the next recognized token in the text. */
+    read_token() {
+        for (const [kind, regex] of this.regexes) {
+            const match = this.try_regex(regex);
+            if (match === null) {
+                continue;
+            }
+            const out = {
+                kind,
+                value: match[0],
+                line: this.line,
+                col: this.col,
+            };
+
+            // handle line/column
+            if (kind === 'newline') {
+                this.lines.push(this.index);
+                this.line++;
+                this.col = 1;
+                return out;
+            }
+            this.col += match[0].length;
+
+            return out;
+        }
+        return {
+            kind: 'end of file',
+            value: '',
+            line: this.line,
+            col: this.col,
+        };
+    }
+
+    /** peeks the next token. the same token can be consumed using next(). */
+    peek() {
+        if (this.current_token === null) {
+            this.current_token = this.read_token();
+        }
+        return this.current_token;
+    }
+
+    /** returns the next token, then sets the peeked token to null. */
+    next() {
+        const tmp = this.peek();
+        this.current_token = null;
+        return tmp;
+    }
+
+    /**
+     * skip all tokens where cond returns false.
+     * returns the first token where cond returns true.
+     * may return end of file
+     */
+    skip_until(cond) {
+        while (true) {
+            const next = this.peek();
+            if (cond(next) || next.kind === 'end of file') {
+                return next;
+            }
+            this.next();
+        }
+    }
+
+    /** skip_until but with an inverted cond */
+    skip_while(cond) {
+        while (true) {
+            const next = this.peek();
+            if (!cond(next) || next.kind === 'end of file') {
+                return next;
+            }
+            this.next();
+        }
+    }
+
+    /** returns the first token whose kind is in the expected array */
+    peek_until = expected =>
+        this.skip_until(next => expected.includes(next.kind));
+
+    /** returns the first token whose kind isn't in the skippable array */
+    peek_after = skippable =>
+        this.skip_while(next => skippable.includes(next.kind));
+}
---
+++ parser.js
@@ -0,0 +1,138 @@
+const WS = ['comment', 'whitespace'];
+const WS_NL = WS.concat('newline');
+
+class Parser {
+    lexer;
+    constructor(text) {
+        this.lexer = new Lexer(text);
+    }
+
+    err_queue = [];
+
+    /**
+     * `context` determines how verbose the error is.
+     * context being null just prints the message.
+     * context being [x, y] prints:
+     *   `Error on <line> <col>: {message}, found <token>`,
+     *   then x lines before the error,
+     *   then the error line,
+     *   then an underline under the bad token,
+     *   then y lines after.
+     *
+     * FOOTGUN: using lexer.next() will make lexer.current_token === null
+     * which makes lexer.queue_err_here() choke without a given token
+     */
+    queue_err_here(
+        message,
+        context = [3, 2],
+        token = this.lexer.current_token
+    ) {
+        if (token === null) {
+            throw 'ATTEMPTED TO QUEUE ERROR WITH NULL TOKEN.\n\
+                This was probably caused by using lexer.next() before lexer.queue_err_here();\n\
+                Consider providing a token beforehand.';
+        }
+        this.err_queue.push({ message, context, token });
+    }
+
+    /** converts an err command into actual text and sends it to `raise_err` */
+    raise_err_with(raise_err, err) {
+        const { message, context, token } = err;
+        const { length, line, col, value } = this.lexer.token_info(token);
+
+        let error_msg = '';
+        if (context) {
+            error_msg += `Error on line ${line}, columns ${col}-${
+                col + length - 1
+            }: `;
+        }
+        error_msg += `${message}`;
+        if (context) {
+            error_msg += `, found ${value}`;
+            const [pre, post] = context;
+            // `line` starts from 1, get_context_lines uses a 0-indexed exclusive range
+            const pre_context = this.lexer.get_context_lines(
+                line - pre - 1,
+                line
+            );
+            let post_context = this.lexer.get_context_lines(line, line + post);
+            if (post_context) {
+                post_context = '\n' + post_context;
+            }
+            const highlight_arrows = ' '.repeat(col - 1) + '^'.repeat(length);
+            error_msg += `\n${pre_context}\n${highlight_arrows}${post_context}`;
+        }
+        error_msg += '\n';
+        raise_err(error_msg);
+    }
+
+    flush_errs(raise_err) {
+        for (const err of this.err_queue) {
+            this.raise_err_with(raise_err, err);
+        }
+        this.err_queue = [];
+    }
+
+    /** intended to be used immediately after the '@RULE' token. */
+    parse_section_rule() {
+        if (this.lexer.next().value !== '@RULE') {
+            throw 'this function was used incorrectly.';
+        }
+
+        let name = 'UNNAMED';
+
+        let token = this.lexer.peek_after(WS);
+        if (token.kind === 'identifier') {
+            name = token.value;
+        } else {
+            this.queue_err_here('expected rule name (identifier) after @RULE');
+        }
+        if (this.lexer.peek_after(WS).kind !== 'newline') {
+            this.queue_err_here(
+                'expected nothing but a newline after rule name'
+            );
+        }
+        this.lexer.peek_after(WS_NL);
+
+        return { kind: 'rule', name };
+    }
+
+    find_next_header() {
+        if (this.lexer.peek_after(WS_NL).kind !== 'header') {
+            this.queue_err_here('expected header, such as @RULE or @TABLE');
+        }
+        return this.lexer.peek_until(['header']);
+    }
+
+    parse_section() {
+        switch (this.find_next_header()) {
+            case '@RULE':
+                return this.parse_section_rule();
+            default:
+                this.lexer.next();
+                return { kind: 'unknown' };
+        }
+    }
+
+    parse_top_level() {
+        let sections = [];
+        while (this.lexer.unfinished()) {
+            sections.push(this.parse_section());
+        }
+        return {
+            kind: 'file',
+            sections,
+        };
+    }
+
+    finish(raise_err) {
+        this.lexer.finish();
+        this.flush_errs(raise_err);
+    }
+
+    parse(raise_err) {
+        const out = this.parse_top_level();
+        this.finish(raise_err);
+        return out;
+    }
+}
